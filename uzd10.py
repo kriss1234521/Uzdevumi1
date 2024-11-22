0a64f5c9826fe8871b73ce9c62bfc352e6295948
kriss1234521 <kriss.mikitans@gmail.com>
2024-11-22 09:34:57 +0200

    uzd10.py

--- uzd10.py
+++ uzd10.py
@@ -1,11 +1,11 @@
-from transformers import MarianMTModel, MarianTokenizer
+from googletrans import Translator
 
-src_texts = ["Labdien! Kā jums klājas?", "Es šodien lasīju interesantu grāmatu."]
-model_name = "Helsinki-NLP/opus-mt-lv-en"
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-model = MarianMTModel.from_pretrained(model_name)
+translator = Translator()
+texts = [
+    "Labdien! Kā jums klājas?",
+    "Es šodien lasīju interesantu grāmatu."
+]
 
-for text in src_texts:
-    inputs = tokenizer(text, return_tensors="pt", padding=True)
-    translated = model.generate(**inputs)
-    print(f"Izvade: {tokenizer.decode(translated[0], skip_special_tokens=True)}")
+for text in texts:
+    translated = translator.translate(text, src='lv', dest='en')
+    print(translated.text)
