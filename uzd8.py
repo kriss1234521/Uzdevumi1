58905c467935bdcf07a29243c1fefca8aaed6b9c
kriss1234521 <kriss.mikitans@gmail.com>
2024-11-22 09:33:37 +0200

    uzd8.py

--- uzd8.py
+++ uzd8.py
@@ -1,8 +1,8 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("lv_core_news_sm")
 text = "Valsts prezidents Egils Levits piedalījās pasākumā, ko organizēja Latvijas Universitāte."
-
 doc = nlp(text)
+
 for ent in doc.ents:
-    print(f"Vienība: {ent.text} - Tips: {ent.label_}")
+    print(ent.text, ent.label_)
